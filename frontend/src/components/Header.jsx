3cd5fbee91a1921da842f56439104d97b1bc2292
wang-chonghuan <wangchonghuan@gmail.com>
2023-06-06 17:03:01 +0100

    add navi bar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,32 @@
+import { Link, NavLink } from "react-router-dom";
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav className="flex justify-between relative items-center font-mono h-16">
+      <Link to="/" className="pl-8 text-xl font-bold">Cars FARM</Link>
+      <div className="pr-8 font-semibold">
+
+        <NavLink className={({ isActive }) => {
+          return isActive ? "active-link" : "p-4"
+        }} to="/">
+          Home
+        </NavLink>
+
+        <NavLink className={({ isActive }) => {
+          return isActive ? "active-link" : "p-4"
+        }} to="/cars">
+          Cars
+        </NavLink>
+
+        <NavLink className={({ isActive }) => {
+          return isActive ? "active-link" : "p-4"
+        }} to="/new">
+          New Car
+        </NavLink>
+
+      </div>
+    </nav>
   )
 }
+
 export default Header
